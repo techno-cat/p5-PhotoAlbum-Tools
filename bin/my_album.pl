59138e6b1395a39f8535d4c3c094510d962b36b0
techno-cat <techno.cat.miau@gmail.com>
2013-06-13 03:58:43 +0900

    アルバム出力スクリプトの更新
     - XslateによるHTMLの一部を実装

--- bin/my_album.pl
+++ bin/my_album.pl
@@ -2,6 +2,10 @@ use strict;
 use warnings;
 use utf8;
 use PhotoAlbum::Tools;
+use Text::Xslate;
+use FindBin qw($Bin);
+use Encode;
+use File::Spec;
 
 use Getopt::Long qw/:config posix_default no_ignore_case bundling auto_help/;
 use Pod::Usage qw/pod2usage/;
@@ -20,20 +24,20 @@ use Pod::Usage qw/pod2usage/;
         dir => '/Users/(User Name)/foo/bar',
         ignore => [ 'private' ],    # not Required
         rule => '[\d]{8}',          # not Required
-        thumb => [
-            {   # Large
+        thumb => {
+            large => {
                 dir => '/Users/(User Name)/hoge/large',
                 width => 1200
             },
-            {   # Midium
+            midium => {
                 dir => '/Users/(User Name)/hoge/midium',
                 width => 400
             },
-            {   # Small
+            small => {
                 dir => '/Users/(User Name)/hoge/small',
                 width => 100
             }
-        ]
+        }
     };
 
 =cut
@@ -101,18 +105,22 @@ sub main {
     my %write_logs = ();
     foreach my $dir_YYYY (sort(@dir_YYYY_ary)) {
         my @dir_YYYYMMDD_ary = PhotoAlbum::Tools::get_photo_dir({
-            dir  => join( '/', $config->{dir}, $dir_YYYY ),
+            dir  => File::Spec->catdir( $config->{dir}, $dir_YYYY ),
             rule => $dir_YYYY . '[\d]{4}',
             ignore => ( exists $config->{ignore} ) ? $config->{ignore} : []
         });
 
         foreach my $dir_YYYYMMDD (sort(@dir_YYYYMMDD_ary)) {
-            my @thumb_settings = map +{
-                dir => join( '/', $_->{dir}, $dir_YYYY, $dir_YYYYMMDD ),
-                width => $_->{width}
-            }, @{$config->{thumb}};
+            my @thumb_settings = map {
+                my $setting = $config->{thumb}->{$_};
+                {
+                    key   => $_,
+                    dir   => File::Spec->catdir( $setting->{dir}, $dir_YYYY, $dir_YYYYMMDD ),
+                    width => $setting->{width}
+                };
+            } keys %{$config->{thumb}};
 
-            my $photo_dir = join( '/', $config->{dir}, $dir_YYYY, $dir_YYYYMMDD );
+            my $photo_dir = File::Spec->catdir( $config->{dir}, $dir_YYYY, $dir_YYYYMMDD );
             my $write_log = PhotoAlbum::Tools::write_thumb({
                 dir => $photo_dir,
                 thumb => \@thumb_settings
@@ -122,11 +130,47 @@ sub main {
         }
     }
 
-    print 'write_logs count: ', scalar(keys %write_logs), "\n";
-    foreach my $key (keys %write_logs) {
-        my $cnt = scalar(@{$write_logs{$key}});
-        $key =~ s/$config->{dir}//;
-        printf( "%s => %3d\n", $key, $cnt );
+    write_photo_album({
+        photo_dir => $config->{dir},
+        thumb     => $config->{thumb},
+        templ_dir => "$Bin/template",
+        templates => {
+            'index'     => 'index.tx',
+            'YYYY'      => 'YYYY.tx',
+            'YYYYMMDD'  => 'YYYYMMDD.tx'
+        },
+        logs => \%write_logs
+    });
+}
+
+sub write_photo_album {
+    my $args = shift;
+
+    my $xslate = Text::Xslate->new(
+        path => [ $args->{templ_dir} ],
+    );
+
+    foreach my $key (keys %{$args->{logs}}) {
+
+        # データ構造の変換
+        my %photo_urls = ();
+        foreach my $write_log (@{$args->{logs}->{$key}}) {
+            my ($volume, $directories, $file) = File::Spec->splitpath( $write_log->{path} );
+            if ( not (exists $photo_urls{$file}) ) {
+                $photo_urls{$file} = {};
+            }
+
+            $photo_urls{$file}->{$write_log->{key}} = $write_log->{path};
+        }
+
+        $key =~ s/$args->{photo_dir}//;
+        my $content = $xslate->render( $args->{templates}->{'YYYY'}, {
+            title => $key,
+            urls  => \%photo_urls
+        });
+
+        print encode_utf8( $content );
+        last;
     }
 }
 
